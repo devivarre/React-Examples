23aa3026747e243aa50881853258e98fed252e1b
Devi <devi.08590@gmail.com>
2019-10-06 10:41:47 +0530

    Initial Commit here

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="jumbotron">
+      <h3> React Example Applications </h3>
+      <ul>
+        <li>Simple Calculator</li>
+        </ul>
+      </div>
+  );
+}
+
+export default App;
